5fb37d9ef9f594206180eac2d70c1b57cdb6da2b
WooSeok <gmn94@naver.com>
2024-09-19 16:56:30 +0900

    Mod Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { EventsGateway } from './events.gateway';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService, EventsGateway],
+  providers: [EventsGateway],
 })
 export class AppModule {}
